35d3442a937e09f762b9bd17ba86ebb37d545cfe
unknown <dev.kubicki@gmail.com>
2024-05-28 20:31:18 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import SingleChart from './components/chartAndProbability/SingleChart';
+import ChartComponenent from './components/chartAndProbability/ChartComponenent';
+
+function App() {
+  return (
+    <div className="App">
+      test
+      <ChartComponenent/>
+    </div>
+  );
+}
+
+export default App;
